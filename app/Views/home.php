66fbc609f23d365be79f331206a89a7e0e540af3
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-05-12 22:09:36 +0530

    removes uer requirement

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,21 +1,8 @@
 <?php echo $this->extend('default'); ?>
 <?php echo $this->section('content'); ?>
 
-<?php if (! user()) { ?>
+Welcome to MaxFlow tools.
 
-    <p>Please <a href="/home/login">Login</a> to continue.</p>
 
-<?php } else { ?>
-    <p class="mt-3"> Welcome 
-        <span class="text-primary"> <?php echo user(); ?>!</span>. 
-        Your roles are <?php echo implode(' & ', userRoles()) ?>.
-    </p> 
-
-    <div class="row d-flex justify-content-between" class="h3">
-        <a class="col btn btn-light m-3" href="/home/logout">Logout</a>
-        <a class="col btn btn-info m-3" href="/ehr">EHR Form</a>
-        <a class="col btn btn-info m-3" href="/trainer">Trainer Home</a>
-    </div>
-<?php } ?>
 
 <?php echo $this->endSection(); ?>
