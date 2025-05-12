7786b84e8a9b6d6e263bf1c9216d30c8b475b8cc
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-05-12 22:58:26 +0530

    Adds routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,9 +1,11 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\ToolImageCompressor;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
 $routes->get('/tool/compress/image', 'ToolImageCompressor::index');
+$routes->post('/tool/compress/action/(:segment)', [[ToolImageCompressor::class, 'handleAction'], '$1']);
--- app/Controllers/ToolImageCompressor.php
+++ app/Controllers/ToolImageCompressor.php
@@ -6,6 +6,17 @@ class ToolImageCompressor extends BaseController
 {
     public function index(): string
     {
-        return view('tools/image_compressor');
+        return $this->loadToolView('image_compressor');
+    }
+
+    public function handleAction(string $actionName): string 
+    {
+        log_message("info", "Handling action $actionName...");
+
+        return $this->loadToolView('image_compressor');
+    }
+
+    private function loadToolView(string $toolName): string {
+        return view("tools/$toolName");
     }
 }
