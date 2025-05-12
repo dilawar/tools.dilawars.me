6725978476e70d06752381cf973a4f001663dc40
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-05-12 22:33:40 +0530

    Adds image compressor

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,6 +1,16 @@
 <?php echo $this->extend('default'); ?>
 <?php echo $this->section('content'); ?>
 
-<div class="display-6"> Welcome to MaxFlow </div>
+<section>
+    <div class="row mx-2">
+        <div class="card col-4">
+          <div class="card-body">
+            <h5 class="card-title">Compress Image</h5>
+            <p class="card-text">Yet another tools to compress images.</p>
+            <a href="/tool/compress/image" class="card-link">Open Image Compressor</a>
+          </div>
+        </div>
+    </div>
+</section>
 
 <?php echo $this->endSection(); ?>
---
+++ app/Views/tools/image_compressor.php
@@ -0,0 +1,11 @@
+<?php
+
+echo $this->extend('default');
+echo $this->section('content');
+
+?>
+
+<h1 class="section-title">Image Compressor </h1>
+
+
+<?php echo $this->endSection(); ?>
