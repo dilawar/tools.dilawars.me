f6c729070c27d053c4bbf25993b7c326131bc0e2
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-05-17 19:24:27 +0530

    Adds translation blocks

--- app/Helpers/view_helper.php
+++ app/Helpers/view_helper.php
@@ -1,2 +1,19 @@
 <?php
 
+function glossaryImageHeic(): string
+{
+    return _glossary(
+        'HEIC',
+        "https://en.wikipedia.org/wiki/High_Efficiency_Image_File_Format"
+    );
+}
+
+function glossaryImageJpeg(): string
+{
+    return _glossary('JPEG', "https://en.wikipedia.org/wiki/JPEG");
+}
+
+function _glossary(string $label, string $url): string {
+    return "<a class='glossary' 
+        target='_blank' href='$url'>$label</a>";
+}
--- app/Views/home.php
+++ app/Views/home.php
@@ -1,8 +1,10 @@
 <?php echo $this->extend('default'); ?>
 <?php echo $this->section('content'); ?>
 
+
 <section>
     <div class="row mx-1">
+        <!--
         <div class="col-4">
             <div class="card mx-1">
                 <div class="card-body">
@@ -12,12 +14,23 @@
                 </div>
             </div>
         </div>
+        -->
         <div class="col-4">
             <div class="card mx-1">
                 <div class="card-body">
-                    <h5 class="card-title">Heic To JPEG</h5>
-                    <p class="card-text">Convert HEIC image to ubiquitous JPEG</p>
-                    <a href="/tool/convert/heic/jpeg" class="card-link">HEIC to JPEG</a>
+                    <h5 class="card-title">Convert HEIC image to JPEG</h5>
+                    <a href="/tool/convert/heic/jpeg" class="btn btn-primary stretched-link">
+                        HEIC to JPEG
+                    </a>
+                </div>
+                <div hidden>
+                    <div>
+                        <p><strong>Hindi (हिन्दी):</strong> HEIC को JPEG में बदलें</p>
+                        <p><strong>Kannada (ಕನ್ನಡ):</strong> HEIC ಅನ್ನು JPEG ಗೆ ಪರಿವರ್ತಿಸಿ</p>
+                        <p><strong>Tamil (தமிழ்):</strong> HEIC-ஐ JPEG-ஆக மாற்றவும்</p>
+                        <p><strong>Telugu (తెలుగు):</strong> HEIC ను JPEG గా మార్చండి</p>
+                        <p><strong>Marathi (मराठी):</strong> HEIC चे JPEG मध्ये रूपांतर करा</p>
+                    </div>
                 </div>
             </div>
         </div>
