f6075b4c6f8a24e1354cda3a5ae3d6ddb62b40ad
Dilawar Singh <dilawar_s@zohomail.in>
2025-12-27 15:07:10 +0530

    fix: initialize logger properly and use sentry (#14)
    * Adds loggers
    
    * Fix error
    
    * Use php-8.4 in actions

--- app/Config/Logger.php
+++ app/Config/Logger.php
@@ -93,56 +93,56 @@ class Logger extends BaseConfig
          * File Handler
          * --------------------------------------------------------------------
          */
-        // FileHandler::class => [
-        //     // The log levels that this handler will handle.
-        //     'handles' => [
-        //         'critical',
-        //         'alert',
-        //         'emergency',
-        //         'debug',
-        //         'error',
-        //         'info',
-        //         'notice',
-        //         'warning',
-        //     ],
+        FileHandler::class => [
+            // The log levels that this handler will handle.
+            'handles' => [
+                'critical',
+                'alert',
+                'emergency',
+                'debug',
+                'error',
+                'info',
+                'notice',
+                'warning',
+            ],
 
-        //     /*
-        //      * The default filename extension for log files.
-        //      * An extension of 'php' allows for protecting the log files via basic
-        //      * scripting, when they are to be stored under a publicly accessible directory.
-        //      *
-        //      * NOTE: Leaving it blank will default to 'log'.
-        //      */
-        //     'fileExtension' => '',
+            //     /*
+            //      * The default filename extension for log files.
+            //      * An extension of 'php' allows for protecting the log files via basic
+            //      * scripting, when they are to be stored under a publicly accessible directory.
+            //      *
+            //      * NOTE: Leaving it blank will default to 'log'.
+            //      */
+            //     'fileExtension' => '',
 
-        //     /*
-        //      * The file system permissions to be applied on newly created log files.
-        //      *
-        //      * IMPORTANT: This MUST be an integer (no quotes) and you MUST use octal
-        //      * integer notation (i.e. 0700, 0644, etc.)
-        //      */
-        //     'filePermissions' => 0644,
+            //     /*
+            //      * The file system permissions to be applied on newly created log files.
+            //      *
+            //      * IMPORTANT: This MUST be an integer (no quotes) and you MUST use octal
+            //      * integer notation (i.e. 0700, 0644, etc.)
+            //      */
+            //     'filePermissions' => 0644,
 
-        //     /*
-        //      * Logging Directory Path
-        //      *
-        //      * By default, logs are written to WRITEPATH . 'logs/'
-        //      * Specify a different destination here, if desired.
-        //      */
-        //     'path' => '',
-        // ],
+            //     /*
+            //      * Logging Directory Path
+            //      *
+            //      * By default, logs are written to WRITEPATH . 'logs/'
+            //      * Specify a different destination here, if desired.
+            //      */
+            //     'path' => '',
+        ],
 
         /*
          * The ChromeLoggerHandler requires the use of the Chrome web browser
          * and the ChromeLogger extension. Uncomment this block to use it.
          */
-        // 'CodeIgniter\Log\Handlers\ChromeLoggerHandler' => [
-        //     /*
-        //      * The log levels that this handler will handle.
-        //      */
-        //     'handles' => ['critical', 'alert', 'emergency', 'debug',
-        //                   'error', 'info', 'notice', 'warning'],
-        // ],
+        \CodeIgniter\Log\Handlers\ChromeLoggerHandler::class => [
+            /*
+             * The log levels that this handler will handle.
+             */
+            'handles' => ['critical', 'alert', 'emergency', 'debug',
+                'error', 'info', 'notice', 'warning'],
+        ],
 
         /*
          * The ErrorlogHandler writes the logs to PHP's native `error_log()` function.
@@ -151,12 +151,6 @@ class Logger extends BaseConfig
         \CodeIgniter\Log\Handlers\ErrorlogHandler::class => [
             /* The log levels this handler can handle. */
             'handles' => ['critical', 'alert', 'emergency', 'debug', 'error', 'info', 'notice', 'warning'],
-
-            /*
-            * The message type where the error should go. Can be 0 or 4, or use the
-            * class constants: `ErrorlogHandler::TYPE_OS` (0) or `ErrorlogHandler::TYPE_SAPI` (4)
-            */
-            'messageType' => 0,
         ],
     ];
 }
